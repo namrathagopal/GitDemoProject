fc0fdbfc8c1d8227fca74312fe104f1557b16bcd
Namratha <namrathag23.com>
2021-05-27 16:44:36 +0530

    Given my 1st commit!

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		
+		if(hungry=="yes") {
+			System.out.println("EAT!!!!!");
+		}
+		
+		else
+			System.out.println("Do some work then!!");
+
+	}
+
+}
