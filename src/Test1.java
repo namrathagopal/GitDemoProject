66fe999e03810cb77c174376f77c7c33f763eb48
Namratha <namrathag23.com>
2021-05-27 16:51:09 +0530

    Added the additional checks!!

--- src/Test1.java
+++ src/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		String hungry="yes";
 		
-		if(hungry=="yes") {
+		if(hungry=="yes" || hungry=="y" || hungry=="S" || hungry=="YES" || hungry=="Y") {
 			System.out.println("EAT!!!!!");
 		}
 		
